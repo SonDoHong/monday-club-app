5d58faf65c28af901598eff92206cb872ab01183
Son Do <son0963433576@gmail.com>
2024-09-24 14:59:59 +0700

    update

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -22,5 +23,6 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
+export const storage = getStorage(app);
 
 export default db
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import './Home.css'
 
 function Home() {
     return <div className="wrapper_home">
-        <img className="home_img" src="src\assets\img\image.png" alt="" />
+        <img className="home_img" src="public\image.png" alt="" />
     </div>;
 }
 
