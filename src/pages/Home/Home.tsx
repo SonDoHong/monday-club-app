a5e99c0adc31524ea8d3c47cd1e7b592c7aecde0
Son Do <son0963433576@gmail.com>
2024-09-24 15:02:15 +0700

    update

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import './Home.css'
 
 function Home() {
     return <div className="wrapper_home">
-        <img className="home_img" src="public\image.png" alt="" />
+        <img className="home_img" src="https://scontent.fhan14-2.fna.fbcdn.net/v/t39.30808-6/414652935_2210032215865906_9065475304341335285_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=2285d6&_nc_ohc=kffMbnMUeBwQ7kNvgF7udku&_nc_ht=scontent.fhan14-2.fna&_nc_gid=Av8oQFkEvdKHIRqFpEuGoOz&oh=00_AYBO3Q9GKPrxjAVVLiaeRKgtlxQVBhrx6tKpZJBKl-rFAg&oe=66F85C19" alt="" />
     </div>;
 }
 
