4fea0405a3032c57a611bba67b8682acf3a734ed
Son Do <son0963433576@gmail.com>
2024-09-24 13:04:35 +0700

    update

--- src/components/Layout/components/Header/Header.tsx
+++ src/components/Layout/components/Header/Header.tsx
@@ -31,13 +31,13 @@ function Header() {
                     v3
                 </NavLink>
 
-                {false && <NavLink
+                <NavLink
                     style={{display: 'none'}}
                     className={({ isActive }) => (isActive ? 'active' : 'nav_item')}
                     to={config.routes.admin}
                 >
                     Admin
-                </NavLink>}
+                </NavLink>
             </div>
         </div>
     );
